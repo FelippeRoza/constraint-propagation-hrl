87f8f6bae2308d9457fb1db0e21c06c734f664af
Felippe Roza <FelippeRoza@users.noreply.github.com>
2025-12-26 23:15:58 +0100

    adding project root folder to path

--- training/train_hrl.py
+++ training/train_hrl.py
@@ -6,6 +6,10 @@ import yaml
 from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.policies import ActorCriticPolicy
+
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from environments.safety_wrappers import make_safety_env
 from models.custom_feature_extractor import CustomMultimodalFeatureExtractor
 
